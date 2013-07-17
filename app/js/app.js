e49ced712425003338bd241593e3b7869cccbae9
eric mcgregor <eric.mcgregor@rackspace.com>
2013-07-17 09:21:25 -0500

    dynamic templates?
    i think so

--- app/js/app.js
+++ app/js/app.js
@@ -21,7 +21,9 @@ var appConfig = function($routeProvider) {
 		controller: 'AppCtrl',
 		templateUrl: 'app/view/guide.php',
 		resolve: {
-			guides: appCtrl.loadData
+			guides: appCtrl.loadData,
+			templates: appCtrl.loadTemplates
+
 		}
 	})
 
@@ -30,30 +32,130 @@ var App = angular.module('App', ['ui.bootstrap', 'ngResource', 'ngSanitize']).co
 
 
 
-var appCtrl = App.controller('AppCtrl', function($scope, $route, $routeParams, $q, Catalogue){
-	// console.log($route);
-	Catalogue.guides;
+var appCtrl = App.controller('AppCtrl', function($scope, $q, Catalogue){
+	$scope.catalogue = Catalogue;
 })
 
-appCtrl.loadData = function($q, $http) {
+appCtrl.loadData = function($q, $http, $route, Catalogue) {
 	var defer = $q.defer();
 	$http.get('app/api/get-guides.php').success(function(data){
+		Catalogue.guides = data;
+		Catalogue.guide = Catalogue.guides[$route.current.params.guideIndex];
 		defer.resolve(data);
 	})
 	return defer.promise;
 }
 
-App.factory('Catalogue', function($route, $routeParams){
-	 var guides = $route.current.locals.guides;
-	 var guide = guides[$routeParams.guideIndex];
+appCtrl.loadTemplates = function($q, $http, $route, Catalogue) {
+	var chapter = $q.defer();
+	$http.get('app/view/templates/chapter.html').success(function(data){
+		Catalogue.templates['chapter'] = data;
+		chapter.resolve(data);
+	});
+
+	var book = $q.defer();
+	$http.get('app/view/templates/book.html').success(function(data){
+		Catalogue.templates['book'] = data;
+		chapter.resolve(data);
+	});
+	
 	return {
-		guides: function() {
-			console.log('test')
-			 return this.guides;
+		chapter: chapter.promise,
+		book: book.promise
+	}
+}
+
+App.factory('Catalogue', function($http, $route, $routeParams){
+	return {
+		guides: [],
+		guide: {},
+		saveGuide: function(){
+			console.log(angular.toJson(this.guide))
+			this.guide.id = this.guide._id.$id;
+			$http.get('app/api/save-guide.php', {
+				params:{
+					json:angular.toJson(this.guide)
+				}
+			});
 		},
-		guide: function() {
-			return this.guide;
+		newGuide: function(){
+			
+		},
+		deleteGuide: function(){
+
+		},
+		edit: {},
+		copy:{},
+		templates:{}
+	}
+});
+
+
+
+App.directive('clgEditor', function($templateCache, $compile, Catalogue) {
+	return {
+		scope: {},
+		controller: function($scope, $element, $attrs, $http) {
+			$scope.catalogue = Catalogue;
+
+		},
+		link: function($scope, $element, $attrs, controller) {
+			
+			$scope.$on('editItem', function(){
+				var type = $scope.catalogue.edit.type;
+				$element.html($scope.catalogue.templates[type]);
+
+				console.log($scope.catalogue.templates[type])
+
+				// $element.html('<editor-content></editor-content>');
+
+
+				$compile($element.contents())($scope);
+				$scope.$apply();
+			});
+
 		}
 	}
-
 });
+App.directive('editorContent', function(Catalogue, $templateCache){
+	return {
+		scope:{},
+		restrict:'E',
+		require:"^clgEditor",
+		// template:'<input type="text" ng-model="catalogue.edit.title"> <button class="btn btn-primary btn-small" ng-click="catalogue.saveGuide()">save</button>',
+		// template:$templateCache.get('test'),
+		link: function(scope, element, attrs, editorCtrl) {
+			scope.catalogue = Catalogue;
+			console.log($templateCache.get('test'));
+
+		}
+	}
+});
+
+
+App.directive('editItem', function(Catalogue, $rootScope){
+	return {
+		scope: {
+			item:'='
+		},
+		link: function(scope, element, attrs, clgEditorCtrl){
+			element.bind('click', function(){
+				Catalogue.edit = scope.item;
+				$rootScope.$broadcast('editItem');
+			});
+		}
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/view/guide.php
+++ app/view/guide.php
@@ -8,13 +8,25 @@
 
 
 <div class="span4">
+	<input type="text" ng-model="catalogue.guide.title"> <button class="btn btn-primary btn-small" ng-click="catalogue.saveGuide()">save</button>
+	<div ng-repeat="book in catalogue.guide.books">
+			<div edit-item item="book">edit - {{item.title}}</div>
+			<br><br><br>
+			<div ng-repeat="chapter in book.chapters">
+				<div edit-item item="chapter">edit - {{item.title}}</div>
 
+			</div>
+		<hr>
+	</div>
 </div>
 
 
 
-<div class="span8" ng-show="showEditor">
+<div class="span8">
+	<div clg-editor>
 
+		<editor-content>asfd</editor-content>
+	</div>
 </div>
 
 
