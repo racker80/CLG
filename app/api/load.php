3ce9429d4b2e469daa6f3566a5b382952a549d24
eric mcgregor <eric.mcgregor@rackspace.com>
2013-08-07 15:21:52 -0500

    trying to fix some issue on the server with the db
    :(

--- app/api/load.php
+++ app/api/load.php
@@ -2,103 +2,103 @@
 header('Content-Type: application/json');
 
 
-// jSON URL which should be requested
-$json_url = 'http://192.237.165.197/CLG/app/api/?action=getAll';
-// jSON String for request
+// // jSON URL which should be requested
+// $json_url = 'http://192.237.165.197/CLG/app/api/?action=getAll';
+// // jSON String for request
 
-// Initializing curl
-$ch = curl_init( $json_url );
+// // Initializing curl
+// $ch = curl_init( $json_url );
 
-// Configuring curl options
-$options = array(
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
-);
+// // Configuring curl options
+// $options = array(
+// CURLOPT_RETURNTRANSFER => true,
+// CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
+// );
 
-// Setting curl options
-curl_setopt_array( $ch, $options );
+// // Setting curl options
+// curl_setopt_array( $ch, $options );
  
-// Getting results
-$output =  json_decode(curl_exec($ch)); // Getting jSON result string
+// // Getting results
+// $output =  json_decode(curl_exec($ch)); // Getting jSON result string
 
 
 
 
-//Drop the database
-$m = new MongoClient();
-$db = $m->selectDB('clg');
-
-$collection = new MongoCollection($db, 'guides');
-$collection->drop();
-//Reformat the guide
-foreach($output->guides as $guide) {
-
-	if($guide->books) :
-		$guide->children = $guide->books;
-		unset($guide->books);
-	
-
-		foreach($guide->children as $book) {
-			if($book->chapters):
-				$book->children = $book->chapters;
-				unset($book->chapters);
-			
-
-				foreach($book->children as $chapter) {
-					if($chapter->pages):
-						$chapter->children = $chapter->pages;
-						unset($chapter->pages);
-					endif;
-				}
-
-			else:
-				continue;
-			endif;
-
-
-		}
-		//REFORMAT DONE
-
-
-
-		//insert the guide	
-		$guide->_id = new MongoId($guide->id);
-		$collection->insert($guide, array("safe" => false));
-
-	endif;
-
-
-}
-
-
-
-
+// //Drop the database
 // $m = new MongoClient();
 // $db = $m->selectDB('clg');
 
-
-
-// $json = json_decode(file_get_contents("db.json"));
-
-
-// $pages = $json->pages;
-// $collection = new MongoCollection($db, 'content');
+// $collection = new MongoCollection($db, 'guides');
 // $collection->drop();
-// foreach($pages as $page) {
+// //Reformat the guide
+// foreach($output->guides as $guide) {
 
-// 	$page->_id = new MongoId($page->id);
+// 	if($guide->books) :
+// 		$guide->children = $guide->books;
+// 		unset($guide->books);
+	
 
-// 	$collection->insert($page, array("safe" => false));
+// 		foreach($guide->children as $book) {
+// 			if($book->chapters):
+// 				$book->children = $book->chapters;
+// 				unset($book->chapters);
+			
+
+// 				foreach($book->children as $chapter) {
+// 					if($chapter->pages):
+// 						$chapter->children = $chapter->pages;
+// 						unset($chapter->pages);
+// 					endif;
+// 				}
+
+// 			else:
+// 				continue;
+// 			endif;
+
+
+// 		}
+// 		//REFORMAT DONE
+
+
+
+// 		//insert the guide	
+// 		$guide->_id = new MongoId($guide->id);
+// 		$collection->insert($guide, array("safe" => false));
+
+// 	endif;
+
+
 // }
 
 
 
-// $guides = $json->guides;
-// $collection = new MongoCollection($db, 'guides');
-// $collection->drop();
-// foreach($guides as $guide) {
-// 	$guide->_id = new MongoId($guide->id);
-// 	$collection->insert($guide, array("safe" => false));
+
+$m = new MongoClient();
+$db = $m->selectDB('clg');
+
+
+
+$json = json_decode(file_get_contents("db.json"));
+
+
+$pages = $json->pages;
+$collection = new MongoCollection($db, 'content');
+$collection->drop();
+foreach($pages as $page) {
+
+	$page->_id = new MongoId($page->id);
+
+	$collection->insert($page, array("safe" => false));
+}
+
+
+
+$guides = $json->guides;
+$collection = new MongoCollection($db, 'guides');
+$collection->drop();
+foreach($guides as $guide) {
+	$guide->_id = new MongoId($guide->id);
+	$collection->insert($guide, array("safe" => false));
 // }	
 
 
